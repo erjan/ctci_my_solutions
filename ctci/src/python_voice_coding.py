ed23d64fdd2a12a207d48f465972644b644d125a
erjan <erjcan@gmail.com>
2016-03-26 16:21:16 +0600

    idea about voice coding python

---
+++ ctci/src/python_voice_coding.py
@@ -0,0 +1,3 @@
+i love coding using voice, cuz my wrists get tunnel syndrome and it hurts to type all the time!
+so i decided to switch to "voice coding", i.e. coding with my pleasant voice, let's see how my productivity will boost and will it boost at all?
+
